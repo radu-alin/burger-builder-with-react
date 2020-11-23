240308ccaa2bc7fa2708fa5ca0dc6bbfc56ec36d
Alin RADU <alin.radu.cristian@gmail.com>
2020-11-23 09:38:58 +0200

    --- finished styling SideDrawer and Toolbar - desktop and mobile

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,16 +1,20 @@
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+import Aux from '../../../hoc/Aux/Aux';
 
 import './SideDrawer.scss';
 
 const Sidedrawer = () => {
   return (
-    <div className="side-drawer p-2 bg-brown-dark">
-      <Logo size={'side-drawer-height'} />
-      <nav>
+    <Aux>
+      <Backdrop show />
+      <header id="SideDrawer" className="side-drawer p-2 bg-brown-dark">
+        <Logo size={'side-drawer-height'} />
         <NavigationItems />
-      </nav>
-    </div>
+      </header>
+    </Aux>
   );
 };
 
